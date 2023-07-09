6ba46aeeb82bcac7255f2b647b29a22ce5c656cf
Felipe Sousa <felipe.fps09@hotmail.com>
2023-07-09 13:55:49 -0300

    Criando deletar, inserir e atualizar nas classes CategoriaController e CategoriaService.

--- backend/src/main/java/com/generationbrasil/daterrapramesa20/controllers/CategoriaController.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/controllers/CategoriaController.java
@@ -4,11 +4,10 @@ import com.generationbrasil.daterrapramesa20.dto.CategoriaDTO;
 import com.generationbrasil.daterrapramesa20.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,8 +23,25 @@ public class CategoriaController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id){
+    public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok().body(categoriaService.categoriaPorId(id));
     }
 
+    @PostMapping
+    public ResponseEntity<CategoriaDTO> inserindoNovaCategoria(@RequestBody CategoriaDTO dto) {
+        dto = categoriaService.inserindoNovaCategoria(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoriaDTO> atualizarCategoria(@PathVariable Long id, @RequestBody CategoriaDTO dto) {
+        return ResponseEntity.ok().body(categoriaService.atualizarCategoria(id, dto));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deletarCategoria(@PathVariable Long id) {
+        categoriaService.deletarCategoriaPorId(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/generationbrasil/daterrapramesa20/services/CategoriaService.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.generationbrasil.daterrapramesa20.services;
 
 import com.generationbrasil.daterrapramesa20.dto.CategoriaDTO;
+import com.generationbrasil.daterrapramesa20.entities.Categoria;
 import com.generationbrasil.daterrapramesa20.repositories.CategoriaRepository;
 import com.generationbrasil.daterrapramesa20.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,33 @@ public class CategoriaService {
     private CategoriaRepository categoriaRepository;
 
     @Transactional(readOnly = true)
-    public List<CategoriaDTO> listarCategorias(){
+    public List<CategoriaDTO> listarCategorias() {
         return categoriaRepository.findAll().stream().map(CategoriaDTO::new).toList();
     }
 
     @Transactional(readOnly = true)
     public CategoriaDTO categoriaPorId(Long id) {
-        return new CategoriaDTO(categoriaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada " + id)));
+        return new CategoriaDTO(categoriaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Não encontrado o Id:" + id)));
     }
+
+    @Transactional
+    public CategoriaDTO inserindoNovaCategoria(CategoriaDTO dto) {
+        Categoria categoria = new Categoria();
+        categoria.setNome(dto.getNome());
+        categoria = categoriaRepository.save(categoria);
+        return new CategoriaDTO(categoria);
+    }
+
+    @Transactional
+    public CategoriaDTO atualizarCategoria(Long id, CategoriaDTO dto) {
+        Categoria categoria = categoriaRepository.getReferenceById(id);
+        categoria.setNome(dto.getNome());
+        categoria = categoriaRepository.save(categoria);
+        return new CategoriaDTO(categoria);
+    }
+
+    public void deletarCategoriaPorId(Long id) {
+        categoriaRepository.deleteById(id);
+    }
+
 }
