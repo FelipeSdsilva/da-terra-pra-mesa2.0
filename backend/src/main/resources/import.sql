99adf2869a005fdac419842bcdd48f0d6a189aa4
Felipe Sousa <felipe.fps09@hotmail.com>
2023-07-09 10:54:14 -0300

    Semeando banco de dados com dados mockados

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_categoria (nome) VALUES ('Grãos e Cereais');
+INSERT INTO tb_categoria (nome) VALUES ('Frutas Secas e Oleaginosas');
+INSERT INTO tb_categoria (nome) VALUES ('Superfoods');
+INSERT INTO tb_categoria (nome) VALUES ('Snacks Saudáveis');
+INSERT INTO tb_categoria (nome) VALUES ('Bebidas Naturais');
