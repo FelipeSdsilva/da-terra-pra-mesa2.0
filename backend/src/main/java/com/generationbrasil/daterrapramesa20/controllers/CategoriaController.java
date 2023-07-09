e32446dedda0fc684b414ee302618a637bf59562
Felipe Sousa <felipe.fps09@hotmail.com>
2023-07-09 10:53:38 -0300

    Criação de busca categoria por Id

--- backend/src/main/java/com/generationbrasil/daterrapramesa20/controllers/CategoriaController.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/controllers/CategoriaController.java
@@ -5,6 +5,7 @@ import com.generationbrasil.daterrapramesa20.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class CategoriaController {
     @GetMapping
     public ResponseEntity<List<CategoriaDTO>> listarTodasAsCategorias() {
         return ResponseEntity.ok().body(categoriaService.listarCategorias());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoriaDTO> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.ok().body(categoriaService.categoriaPorId(id));
     }
 
 }
--- backend/src/main/java/com/generationbrasil/daterrapramesa20/services/CategoriaService.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/services/CategoriaService.java
@@ -1,11 +1,14 @@
 package com.generationbrasil.daterrapramesa20.services;
 
 import com.generationbrasil.daterrapramesa20.dto.CategoriaDTO;
+import com.generationbrasil.daterrapramesa20.entities.Categoria;
 import com.generationbrasil.daterrapramesa20.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -13,7 +16,15 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Transactional(readOnly = true)
     public List<CategoriaDTO> listarCategorias(){
         return categoriaRepository.findAll().stream().map(CategoriaDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoriaDTO categoriaPorId(Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        Categoria cat = categoria.get();
+        return new CategoriaDTO(cat);
     }
 }
