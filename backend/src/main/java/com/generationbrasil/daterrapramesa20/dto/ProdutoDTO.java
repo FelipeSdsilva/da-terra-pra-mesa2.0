dc925cccae13d1c267bac93d2816748efd6ac217
Felipe Sousa <felipe.fps09@hotmail.com>
2023-07-23 09:06:27 -0300

    Relacionando muito para muito com a categoria e acrescentando mais um atributo na classe produto.

--- backend/src/main/java/com/generationbrasil/daterrapramesa20/dto/ProdutoDTO.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/dto/ProdutoDTO.java
@@ -4,6 +4,8 @@ import com.generationbrasil.daterrapramesa20.entities.Produto;
 import org.springframework.beans.BeanUtils;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ProdutoDTO implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -13,20 +15,25 @@ public class ProdutoDTO implements Serializable {
     private String descricao;
     private Double preco;
     private Integer qtd;
+    private String imgUrl;
+
+
+    private Set<CategoriaDTO> categorias = new HashSet<>();
 
     public ProdutoDTO() {
     }
 
-    public ProdutoDTO(Long id, String nome, String descricao, Double preco, Integer qtd) {
+    public ProdutoDTO(Long id, String nome, String descricao, Double preco, Integer qtd, String imgUrl) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.preco = preco;
         this.qtd = qtd;
+        this.imgUrl = imgUrl;
     }
 
     public ProdutoDTO(Produto entity) {
-        BeanUtils.copyProperties(entity,this);
+        BeanUtils.copyProperties(entity, this);
     }
 
     public Long getId() {
@@ -67,5 +74,17 @@ public class ProdutoDTO implements Serializable {
 
     public void setQtd(Integer qtd) {
         this.qtd = qtd;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Set<CategoriaDTO> getCategorias() {
+        return categorias;
     }
 }
