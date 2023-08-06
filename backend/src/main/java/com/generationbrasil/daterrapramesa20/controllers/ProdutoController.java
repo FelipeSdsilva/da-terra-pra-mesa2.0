9f1cb51c1219acc85cbfa4acef1f5453a69f039b
Felipe Sousa <felipe.fps09@hotmail.com>
2023-08-06 12:02:11 -0300

    Criando métodos para novoProduto, produtoPorId, produtosPaginados e atualizarProduto.

--- backend/src/main/java/com/generationbrasil/daterrapramesa20/controllers/ProdutoController.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/controllers/ProdutoController.java
@@ -1,9 +1,37 @@
 package com.generationbrasil.daterrapramesa20.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.generationbrasil.daterrapramesa20.dto.ProdutoDTO;
+import com.generationbrasil.daterrapramesa20.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+
+    @GetMapping
+    public ResponseEntity<Page<ProdutoDTO>> recuperarTodosProdutosPaginado(Pageable pageable) {
+        return ResponseEntity.ok().body(service.produtosPaginados(pageable));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProdutoDTO> recuperarProdutoPorId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(service.produtoPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> inserirProduto(@RequestBody ProdutoDTO dto) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(service.novoProduto(dto));
+    }
 }
--- backend/src/main/java/com/generationbrasil/daterrapramesa20/services/ProdutoService.java
+++ backend/src/main/java/com/generationbrasil/daterrapramesa20/services/ProdutoService.java
@@ -1,7 +1,78 @@
 package com.generationbrasil.daterrapramesa20.services;
 
+import com.generationbrasil.daterrapramesa20.dto.CategoriaDTO;
+import com.generationbrasil.daterrapramesa20.dto.ProdutoDTO;
+import com.generationbrasil.daterrapramesa20.entities.Categoria;
+import com.generationbrasil.daterrapramesa20.entities.Produto;
+import com.generationbrasil.daterrapramesa20.repositories.CategoriaRepository;
+import com.generationbrasil.daterrapramesa20.repositories.ProdutoRepository;
+import com.generationbrasil.daterrapramesa20.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ProdutoDTO> produtosPaginados(Pageable pageable) {
+        return produtoRepository.findAll(pageable).map(ProdutoDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ProdutoDTO produtoPorId(Long id) {
+        return new ProdutoDTO(produtoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Id não encontrado" + id)));
+    }
+
+    @Transactional
+    public ProdutoDTO novoProduto(ProdutoDTO dto) {
+        Produto produto = new Produto();
+        converterDtoEmEntidade(produto, dto);
+        produto = produtoRepository.save(produto);
+        return new ProdutoDTO(produto);
+    }
+
+
+    @Transactional
+    public ProdutoDTO atualizarProduto(Long id, ProdutoDTO dto) {
+        try {
+
+            Produto produto = produtoRepository.getReferenceById(id);
+            converterDtoEmEntidade(produto, dto);
+            produto = produtoRepository.save(produto);
+
+            return new ProdutoDTO(produto);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Produto não encontrado");
+        }
+    }
+
+    public void deletarProduto(Long id){
+
+    }
+
+    private void converterDtoEmEntidade(Produto produto, ProdutoDTO dto) {
+        produto.setNome(dto.getNome());
+        produto.setDescricao(dto.getDescricao());
+        produto.setPreco(dto.getPreco());
+        produto.setQtd(dto.getQtd());
+        produto.setImgUrl(dto.getImgUrl());
+
+        produto.getCategorias().clear();
+
+        for (CategoriaDTO catDto : dto.getCategorias()) {
+            Categoria categoria = categoriaRepository.getReferenceById(catDto.getId());
+            produto.getCategorias().add(categoria);
+        }
+
+    }
 }
